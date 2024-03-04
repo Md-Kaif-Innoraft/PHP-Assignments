35446e6833f2b26f0572c07cd3941549ddca2150
Md-Kaif-Innoraft <kaif.ansari@innoraft.com>
2024-03-04 18:36:33 +0530

    FT2-37-1: Modified features Added input validation and added comments

--- index.php
+++ index.php
@@ -9,72 +9,96 @@
 </head>
 
 <body>
-  <!-- PHP Logic starts here to fetch info  -->
-   
-   <?php
+    <!-- PHP Logic starts here to fetch info  -->
 
-    // Creating variables to store info
-    $fnameErr = $lnameErr = "";
-    $fname = $lname = "";
+    <?php
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["fname"])) {
-            $fnameErr = "* First Name is required";
-        } else {
-            $fname = check_input($_POST["fname"]);
-            if (!preg_match("/^[a-zA-Z-' ]*$/", $fname)) {
-                $fname = "";
-                $fnameErr = "* Only letters and white space allowed";
-            }
+
+        $fnameErr = $lnameErr = "";
+        $fullnameError = "* Invalid Input";
+
+        /*  function to Check user input data. */
+        function check_input($data) {
+            //Removes extra spaces from the begning and end.
+            $data = trim($data);
+            // Removes backslashes 
+            $data = stripslashes($data);
+            // convert special characters to HTML entities
+            $data = htmlspecialchars($data);
+            return $data;
         }
-        if (empty($_POST["lname"])) {
-            $lnameErr = "* Last Name is required";
-        } else {
-            $lname = check_input($_POST["lname"]);
-            if (!preg_match("/^[a-zA-Z-' ]*$/", $lname)) {
-                $lname = "";
-                $lnameErr = "* Only letters and white space allowed";
+        
+        /* Validating user inputs */
+        function validateName($name, &$error, $fieldName) {
+            if (empty($name)) {
+                $error = "* $fieldName Name is required";
+                return "";
+            } 
+            else {
+                $name = check_input($name);
+                if (!preg_match("/^[a-zA-Z-\s' ]*$/", $name)) {
+                    $error = "* Only letters and white space allowed";
+                    return "";
+                } 
+                else {
+                    return $name;
+                }
             }
         }
 
+        $fname = validateName($_POST["fname"], $fnameErr, "First");
+        $lname = validateName($_POST["lname"], $lnameErr, "Last");
+        
     }
 
-    //  function to modify user input data
-    function check_input($data)
-    {
-        $data = trim($data); //removes extra spaces from the begning and end.
-        $data = stripslashes($data);  // removes backslashes 
-        $data = htmlspecialchars($data); // convert special characters to HTML entities
-        return $data;
-    }
     ?>
+
     <div class="container">
         <div class="box">
             <!-- form starts here -->
             <h2 class="text-center">PHP Assignment 1</h2><br><br>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+
                 <label for="fname"><span class="error">* </span> First Name : </label> <br>
-                <input type="text" name="fname" id="fname"> 
-                <span class="error" ><?php echo " $fnameErr <br><br>"; ?> </span>
+                <input type="text" name="fname" id="fname" required maxlength="20" value = "<?php echo $_POST['fname']; ?>" >
+                <span class="error">
+                    <?php echo " $fnameErr <br><br>"; ?>
+                </span>
+
                 <label for="fname"> <span class="error">* </span> Last Name : </label> <br>
-                <input type="text" id="lname" name="lname"> <br>
-                <span class="error"><?php echo "$lnameErr <br><br>"; ?> </span>
+                <input type="text" id="lname" name="lname" required maxlength="20" value = "<?php echo $_POST['lname']; ?>"> <br>
+                <span class="error">
+                    <?php echo "$lnameErr <br><br>"; ?>
+                </span>
+
                 <button class="btn">Submit</button> <br> <br> <br>
+
                 <label for="full_name">Full Name:</label> <br>
-                <input type="text" id="full_name" name="full_name" disabled value = "<?php if(!(empty($fname)|| empty($lname))){
+                <input type="text" id="fullName" name="fullName" disabled value="<?php if (isset($fname) && isset($lname)) {
                     echo "$fname $lname";
                 }
-                ?>"> <br><br>
+                else{
+                    echo "";
+                } ?>">
+                <span class="error">
+                    <?php if(!empty($_POST['fullName'])){
+                        echo $fullnameError;
+                    } ?>
+                </span>
+
+                <br><br>
             </form>
             <!-- form ends here -->
 
             <!-- print full name -->
-
-            <?php  
-                if(!(empty($fname)|| empty($lname))){
-                    echo "<p>Hello, $fname $lname .</p>";
-                }
-                ?>
+                    
+            <?php
+            if (isset($fname) && isset($lname)) {
+                echo "<p>Hello, $fname $lname .</p>";
+            }
+            ?>
         </div>
     </div>
 </body>
