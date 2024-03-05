fa710e4ef1273ee4f999e0d73f33f86989e69100
Md-Kaif-Innoraft <kaif.ansari@innoraft.com>
2024-03-05 18:33:36 +0530

    FT2-37-1: Modified code, Added a new javaScript file for frontend validation Description: Implemented javaScript for validation input fields. Changed index.php file, seprated php and html logic.

---
+++ index.js
@@ -0,0 +1,28 @@
+const regex = /^[a-zA-Z ]+$/;
+
+function validateInputs(inputId, errorId, fieldName, maxLength){
+ 
+    var input = document.getElementById(inputId).value.trim();
+    var error = document.getElementById(errorId);
+
+    if (input == "") {
+        error.innerHTML = "* " + fieldName + " is required.<br>";
+        return false;
+    } else if (!regex.test(input)) {
+        error.innerHTML = "* Only letters and white space allowed for " + fieldName + ".<br><br>";
+        return false;
+    } else if (input.length >= maxLength) {
+        error.innerHTML = "* " + fieldName + " should not contain more than " + maxLength + " characters.<br><br>";
+        return false;
+    }
+    return true;
+}
+
+function validate(){
+
+    var fnameValid = validateInputs("fname", "ferror", "First Name", 20);
+    var lnameValid = validateInputs("lname", "lerror", "Last Name", 20);
+    
+    return fnameValid && lnameValid;
+
+}
--- index.php
+++ index.php
@@ -1,3 +1,61 @@
+<!-- PHP Logic starts here to fetch info  -->
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    /*  Function to remove extra spaces, backslashes and convert special characters to HTML entites form user input data. */
+    function checkInput($data) {
+        //Removes extra spaces from the begning and end.
+        $data = trim($data);
+        // Removes backslashes. 
+        $data = stripslashes($data);
+        // convert special characters to HTML entities.
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    
+    /* Validating user inputs. */
+    function validateName($name, &$error, $fieldName) {
+        if (empty($name)) {
+            $error = "* $fieldName Name is required";
+            return "";
+        } 
+        else {
+            $name = checkInput($name);
+            if (!preg_match("/^[a-zA-Z-\s' ]*$/", $name)) {
+                $error = "* Only letters and white space allowed";
+                return "";
+            } 
+            else if (empty($name)) {
+                $error = "* Empty, Please enter valid $fieldName Name.";
+                return "";
+            }
+            else {
+                return $name;
+            }
+        }
+    }
+
+    // Calling ValidatName function to validate name and store in variable,
+    $fname = validateName($_POST["fname"], $fnameErr, "First");
+    $lname = validateName($_POST["lname"], $lnameErr, "Last");
+
+    //Checking if first name or last name is not empty.
+    if (!empty($fname) && !empty($lname)) {
+        $fullName = "$fname $lname";
+        $message = "Hello $fullName.";
+    }
+
+    //Checking if full name filed is not filled.
+    if(!empty($_POST['fullName'])){
+        $fullnameError = "* Invalid Input, You can't enter any value in Full Name.";
+    }
+        
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,54 +67,6 @@
 </head>
 
 <body>
-    <!-- PHP Logic starts here to fetch info  -->
-
-    <?php
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $fullnameError = "* Invalid Input, You can't enter any value in Full Name.";
-
-        /*  Function to remove extra spaces, backslashes and convert special characters to HTML entites form user input data. */
-        function checkInput($data) {
-            //Removes extra spaces from the begning and end.
-            $data = trim($data);
-            // Removes backslashes. 
-            $data = stripslashes($data);
-            // convert special characters to HTML entities.
-            $data = htmlspecialchars($data);
-            return $data;
-        }
-        
-        /* Validating user inputs. */
-        function validateName($name, &$error, $fieldName) {
-            if (empty($name)) {
-                $error = "* $fieldName Name is required";
-                return "";
-            } 
-            else {
-                $name = checkInput($name);
-                if (!preg_match("/^[a-zA-Z-\s' ]*$/", $name)) {
-                    $error = "* Only letters and white space allowed";
-                    return "";
-                } 
-                else if (empty($name)) {
-                    $error = "* Empty, Please enter valid $fieldName Name.";
-                    return "";
-                }
-                else {
-                    return $name;
-                }
-            }
-        }
-
-        // Calling ValidatName function to validate name and store in variable,
-        $fname = validateName($_POST["fname"], $fnameErr, "First");
-        $lname = validateName($_POST["lname"], $lnameErr, "Last");
-        
-    }
-
-    ?>
 
     <div class="container">
         <div class="box">
@@ -64,30 +74,28 @@
             <!-- form starts here -->
             <h2 class="text-center">PHP Assignment 1</h2><br><br>
 
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+            <form id="form" onsubmit = "return validate()" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
 
                 <label for="fname"><span class="error">* </span> First Name : </label> <br>
-                <input type="text" name="fname" id="fname" required maxlength="20" value = "<?php echo $_POST['fname']; ?>" >
-                <span class="error">
-                    <?php echo " $fnameErr <br><br>"; ?>
+                <input type="text" name="fname" id="fname" required maxlength = "20" value = "<?php echo $_POST['fname']; ?>" pattern = "[a-zA-Z ]*">
+                <span class="error" id="ferror">
+                    <?php echo $fnameErr; ?> <br><br>
                 </span>
 
                 <label for="fname"> <span class="error">* </span> Last Name : </label> <br>
-                <input type="text" id="lname" name="lname" required maxlength="20" value = "<?php echo $_POST['lname']; ?>"> <br>
-                <span class="error">
-                    <?php echo "$lnameErr <br><br>"; ?>
+                <input type="text" id="lname" name="lname" required maxlength = "20" value = "<?php echo $_POST['lname']; ?>" pattern = "[a-zA-Z ]*"> <br>
+                <span class="error" id = "lerror">
+                    <?php echo $lnameErr; ?> <br><br>
                 </span>
 
                 <button class="btn">Submit</button> <br> <br> <br>
 
                 <label for="full_name">Full Name:</label> <br>
-                <input type = "text" id = "fullName" name = "fullName" disabled value = "<?php if (!empty($fname) && !empty($lname)) {
-                    echo "$fname $lname";
-                } ?>">
+                <input type = "text" id = "fullName" name = "fullName" disabled value = "<?php echo $fullName; ?>">
                 <span class="error">
-                    <?php if(!empty($_POST['fullName'])){
+                    <?php 
                         echo $fullnameError;
-                    } ?>
+                    ?>
                 </span>
 
                 <br><br>
@@ -95,14 +103,13 @@
             <!-- form ends here -->
             
             <!-- Printing full name with greeting. -->
-            <?php
-            if (!empty($fname) && !empty($lname)) { ?>
-                <p>Hello, <?php echo "$fname $lname"; ?>.</p>
-            <?php }
+            <?php 
+                echo $message;
             ?>
 
         </div>
     </div>
+    <script src="index.js"></script>
 </body>
 
 </html>
