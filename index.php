c536427eac39281a84957053beb6927f4923d59a
Md-Kaif-Innoraft <kaif.ansari@innoraft.com>
2024-03-05 13:01:16 +0530

    FT2-37-2: Modified Code, added comments, Modified backend validation.

--- index.php
+++ index.php
@@ -12,28 +12,49 @@
     <!-- PHP Logic starts here to fetch info  -->
     <?php
 
-    // Creating variables to store info
-    $fnameErr = $lnameErr = "";
-    $fname = $lname = "";
+    /* Include the User class file. */
+    require_once 'user.php';
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["fname"])) {
-            $fnameErr = "* First Name is required";
-        } else {
-            $fname = check_input($_POST["fname"]);
-            if (!preg_match("/^[a-zA-Z-' ]*$/", $fname)) {
-                $fname = "";
-                $fnameErr = "* Only letters and white space allowed";
+
+        $fullnameError = "* Invalid Input, You can't enter any value in Full Name.";
+
+        /*  Function to remove extra spaces, backslashes and convert special characters to HTML entites form user input data. */
+        function checkInput ($data) {
+            //Removes extra spaces from the begning and end.
+            $data = trim($data);
+            // Removes backslashes. 
+            $data = stripslashes($data);
+            // convert special characters to HTML entities.
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        /* Validating user inputs. */
+        function validateName($name, &$error, $fieldName) {
+            if (empty($name)) {
+                $error = "* $fieldName Name is required";
+                return "";
+            } 
+            else {
+                $name = checkInput($name);
+                if (!preg_match("/^[a-zA-Z-\s' ]*$/", $name)) {
+                    $error = "* Only letters and white space allowed";
+                    return "";
+                } 
+                else if (empty($name)) {
+                    $error = "* Empty, Please enter valid $fieldName Name.";
+                    return "";
+                } 
+                else {
+                    return $name;
+                }
             }
         }
-        if (empty($_POST["lname"])) {
-            $lnameErr = "* Last Name is required";
-        } else {
-            $lname = check_input($_POST["lname"]);
-            if (!preg_match("/^[a-zA-Z-' ]*$/", $lname)) {
-                $lname = "";
-                $lnameErr = "* Only letters and white space allowed";
-            }
-        }
+
+        // Calling ValidatName function to validate name and store in variable,
+        $fname = validateName($_POST["fname"], $fnameErr, "First");
+        $lname = validateName($_POST["lname"], $lnameErr, "Last");
 
         if (!empty($_FILES['image']['name'])) {
             $img_temp_name = $_FILES['image']['tmp_name'];
@@ -41,93 +62,64 @@
             move_uploaded_file($img_temp_name, "uploads/$img_name");
         }
 
-
         //Creating an object 
-    
         $user = new User($fname, $lname);
 
     }
-
-    //  function to modify user input data
-    function check_input($data)
-    {
-        $data = trim($data); //removes extra spaces from the begning and end.
-        $data = stripslashes($data);  // removes backslashes 
-        $data = htmlspecialchars($data); // convert special characters to HTML entities
-        return $data;
-    }
-
-    //creating a class named User
     
-    class User
-    {
-
-        private $fname;
-        private $lname;
-
-        function __construct($fname, $lname)
-        {
-            $this->fname = $fname;
-            $this->lname = $lname;
-
-        }
-
-        function get_Fname()
-        {
-            return $this->fname;
-        }
-
-        function get_Lname()
-        {
-            return $this->lname;
-        }
-
-    }
-
-
-
-
     ?>
+
     <div class="container">
         <div class="box">
+
             <!-- form starts here -->
-            <h2 class="text-center">PHP Assignment 2</h2><br><br>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
-                enctype="multipart/form-data">
+            <h2 class="text-center">PHP Assignment 1</h2><br><br>
+
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
+
                 <label for="fname"><span class="error">* </span> First Name : </label> <br>
-                <input type="text" name="fname" id="fname">
+                <input type="text" name="fname" id="fname" required maxlength="20"
+                    value="<?php echo $_POST['fname']; ?>">
                 <span class="error">
                     <?php echo " $fnameErr <br><br>"; ?>
                 </span>
+
                 <label for="fname"> <span class="error">* </span> Last Name : </label> <br>
-                <input type="text" id="lname" name="lname"> <br>
+                <input type="text" id="lname" name="lname" required maxlength="20"
+                    value="<?php echo $_POST['lname']; ?>"> <br>
                 <span class="error">
                     <?php echo "$lnameErr <br><br>"; ?>
                 </span>
-                Image :
+                <label for="image">Image :</label> <br>
                 <input class="file" type="file" name="image"><br><br>
+
                 <button class="btn">Submit</button> <br> <br> <br>
+
                 <label for="full_name">Full Name:</label> <br>
-                <input type="text" id="full_name" name="full_name" disabled value="<?php if (!(empty($fname) || empty($lname))) {
+                <input type="text" id="fullName" name="fullName" disabled value="<?php if (!empty($fname) && !empty($lname)) {
                     echo "$fname $lname";
-                }
-                ?>"> <br><br>
+                } ?>">
+                <span class="error">
+                    <?php if (!empty($_POST['fullName'])) {
+                        echo $fullnameError;
+                    } ?>
+                </span>
+
+                <br><br>
             </form>
-            <!-- form ends here -->
+            <!-- Form ends here. -->
 
-            <!-- print full name -->
-
+            <!-- Print full name. -->
             <div class="img-sec">
                 <?php
-                if (!(empty($fname) || empty($lname))) {
-                    echo "<p>Hello, {$user->get_Fname()} {$user->get_Lname()} .<br> <br></p>";
-                    if (!empty($img_name)) {
+                if (!(empty($fname) || empty($lname))) { ?>
+                    <p>Hello, <?php echo "{$user->get_Fname()} {$user->get_Lname()}"; ?> .<br> <br></p>
+                   <?php if (!empty($img_name)) {
                         echo "<img src = 'uploads/$img_name' height= 250px >";
                         echo "<br>";
                         echo $img_name;
                     }
-                }
-
+                } 
                 ?>
             </div>
         </div>
