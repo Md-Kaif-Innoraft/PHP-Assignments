a6eeaa3126eef1217dc88112045eb931e549bfc3
Md-Kaif-Innoraft <kaif.ansari@innoraft.com>
2024-03-05 16:32:19 +0530

    FT2-37-6: Modified code, Added validation in html tags

--- index.php
+++ index.php
@@ -13,16 +13,21 @@
     <div class="container">
         <div class="box">
             <!-- form starts here -->
+
             <h2 class="text-center">PHP Assignment 6</h2><br><br>
             <form method="post" action="formpdf.php"enctype="multipart/form-data">
                 <label for="fname"><span class="error">* </span> First Name : </label> <br>
-                <input type="text" name="fname" id="fname">
+                <input type="text" name="fname" id="fname" required maxlength = "20" pattern="[A-Za-z ]*">
+
                 <label for="fname"> <span class="error">* </span> Last Name : </label> <br>
-                <input type="text" id="lname" name="lname"> <br>
+                <input type="text" id="lname" name="lname" required maxlength = "20" pattern="[A-Za-z ]*"> <br>
+
                 <label for="number"><span class="error">* </span> Phone number : </label> <br>
-                <input type="text" id="number" name="number"> <br>
+                <input type="text" id="number" name="number" required maxlength = "10" pattern="[0-9]*"> <br>
+
                 <label for="email"><span class="error">* </span> Email : </label> <br>
-                <input type="text" id="email" name="email"> <br>
+                <input type="email" id="email" name="email" required> <br>
+
                 <label for="sub">Subjects and Marks : ( Format: English|80 )</label>
                 <textarea name="sub" id="sub" rows="3"></textarea>
                 Image :
