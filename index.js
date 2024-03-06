e83b14f81c1c40ca5bd3349f4b5137e6dc32f27f
Md-Kaif-Innoraft <kaif.ansari@innoraft.com>
2024-03-06 16:36:21 +0530

    FT2-37-4: Added JavaScript file for client side validation, added comments , modified code. Description: Added JavaScript file for client side validation. Added comments. Modified codes quality. Separated PHP and HTML codes.

---
+++ index.js
@@ -0,0 +1,37 @@
+const regex = /^[a-zA-Z ]+$/;
+const numberRegex = /^[0-9]+$/;
+
+/* Function to validate user inputs fields */
+function validateInputs(inputId, errorId, fieldName, maxLength, regex){
+ 
+    var input = document.getElementById(inputId).value.trim();
+    var error = document.getElementById(errorId);
+    if (input == "") {
+        error.innerHTML = "* " + fieldName + " is required.<br>";
+        return false;
+    } 
+    else if (!regex.test(input)) {
+        error.innerHTML = "* Invalid Input " + fieldName + ".<br><br>";
+        return false;
+    } 
+    else if (input.length >= maxLength) {
+        error.innerHTML = "* " + fieldName + " should not contain more than " + maxLength + " characters.<br><br>";
+        return false;
+    }
+    return true;
+}
+
+/* Validating user inputs on client side. */
+function validate(){
+
+    var fnameValid = validateInputs("fname", "ferror", "First Name", 20, regex);
+    var lnameValid = validateInputs("lname", "lerror", "Last Name", 20, regex);
+    var number = validateInputs("number", "nerror", "Phone Number", 10, numberRegex) ;
+    if(document.getElementById("number").value.trim().length != 10){
+        number = false;
+        nerror.innerHTML = "*  Phone Number should contain exactly 10 characters.<br><br>";
+    }
+    
+    return fnameValid && lnameValid && number ;
+
+}
