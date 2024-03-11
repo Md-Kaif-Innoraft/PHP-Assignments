75ac2dd3846863c23a93d5435405d273bb382e02
Md-Kaif-Innoraft <kaif.ansari@innoraft.com>
2024-03-11 10:42:15 +0530

    FT2-37-7: Implement login system with session management and question pager functionality. Description: Implement login system with session management and question pager functionality. Upon successful login, users are redirected to the specified question page. Added all the question as page. Additionally, a pager functionality allows navigating through questions, with each question identified by a unique URL parameter.

---
+++ q6.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <h1>Question No. 6.</h1><br>
+    <h2>6. When the user submits the above form, 2 copies of the data will get created in a doc format. One will store
+        on the server and the other will be downloaded to the user submitting the data. The information in the doc
+        should be presented in a well-defined manner.</h2>
+</body>
+
+</html>
