8ee61e9407bb607d9f17627c55a584d1d15b430e
Md-Kaif-Innoraft <kaif.ansari@innoraft.com>
2024-03-13 17:30:22 +0530

    FT2-37-Adv1: Modified index.php for displaying in correct order.

--- index.php
+++ index.php
@@ -16,14 +16,37 @@ $dataObjArr = getData();
     <title>Advanced 1</title>
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
 
     <div class="container">
+    <div class="box">
+
+            <div class="box2">
+            <!-- Displaying image. -->
+            <img src="<?php echo $dataObjArr[count($dataObjArr)-1]->getImage(); ?>" alt="">
+            </div>
+
+            <div class="box1">
+                <!-- Displaying Title.  -->
+                <h2> <?php echo $dataObjArr[count($dataObjArr)-1]->getTitle(); ?></h2>
+
+                <!-- Displaying Service links. -->
+                <?php echo $dataObjArr[0]->getService(); ?> </p>
+
+                <!-- Displaying icons. -->
+                <?php for($j=0; $j<count($dataObjArr[count($dataObjArr)-1]->getIcon()); $j++) {?>
+                <img class="icon" src="<?php echo $dataObjArr[count($dataObjArr)-1]->getIcon()[$j]; ?>" alt="">
+                <?php  } ?> <br><br><br>
+
+                <!-- Explore Button.  -->
+                <a href="<?php echo $dataObjArr[count($dataObjArr)-1]->getAlias(); ?>" class = "btn" >EXPLORE MORE</a>
+            </div>
+            
+        </div>
         <!-- For Loop for iterating through dataObjArr Array. -->
-        <?php for ($i = 0; $i < count($dataObjArr); $i++) { 
+        <?php for ($i = 0; $i < count($dataObjArr)-1; $i++) { 
             // Checking for even and odd to display images and data.
-            if($i % 2 != 0 ) { ?>
+            if($i % 2 == 0 ) { ?>
                 <div class="box">
                 <div class="box1">
 
@@ -36,13 +59,13 @@ $dataObjArr = getData();
                     <!-- Displaying icons. -->
                     <?php for($j=0; $j<count($dataObjArr[$i]->getIcon()); $j++) {?>
                     <img class="icon" src="<?php echo $dataObjArr[$i]->getIcon()[$j]; ?>" alt="">
-                  <?php  } ?> <br><br><br>
+                    <?php  } ?> <br><br><br>
 
                     <!-- Explore Button.  -->
                    <a href="<?php echo $dataObjArr[0]->getAlias(); ?>" class = "btn" >EXPLORE MORE</a>
                 </div>
-                <div class="box2">
 
+                <div class="box2">
                 <!-- Displaying image. -->
                 <img src="<?php echo $dataObjArr[$i]->getImage(); ?>" alt="">
                 </div>
@@ -50,11 +73,12 @@ $dataObjArr = getData();
             <?php } 
             else { ?>
                 <div class="box">
-                <div class="box2">
 
+                <div class="box2">
                 <!-- Displaying image. -->
                 <img src="<?php echo $dataObjArr[$i]->getImage(); ?>" alt="">
                 </div>
+
                 <div class="box1">
                     <!-- Displaying Title.  -->
                     <h2> <?php echo $dataObjArr[$i]->getTitle(); ?></h2>
@@ -67,8 +91,6 @@ $dataObjArr = getData();
                     <img class="icon" src="<?php echo $dataObjArr[$i]->getIcon()[$j]; ?>" alt="">
                     <?php  } ?> <br> <br><br>
                     <a href="<?php echo $dataObjArr[0]->getAlias(); ?>" class = "btn" >EXPLORE MORE</a>
-
-    
                 </div>
             </div>
             <?php
