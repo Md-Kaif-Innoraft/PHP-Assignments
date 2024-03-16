d34bb9665e07e6594830ccb89a04edd33d70a924
Md-Kaif-Innoraft <kaif.ansari@innoraft.com>
2024-03-17 03:37:52 +0530

    FT2-37-1: Modified code changed to oops and added comments according to php cs standards.

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
+/**
+ * @var string @regex.
+ *  Expression to check Alphabets and spaces.
+ */
 const regex = /^[a-zA-Z ]+$/;
 
-/* Function to validate user inputs fields */
-function validateInputs(inputId, errorId, fieldName, maxLength){
- 
-    var input = document.getElementById(inputId).value.trim();
-    var error = document.getElementById(errorId);
-
-    if (input == "") {
-        error.innerHTML = "* " + fieldName + " is required.<br>";
-        return false;
-    } else if (!regex.test(input)) {
-        error.innerHTML = "* Only letters and white space allowed for " + fieldName + ".<br><br>";
-        return false;
-    } else if (input.length >= maxLength) {
-        error.innerHTML = "* " + fieldName + " should not contain more than " + maxLength + " characters.<br><br>";
-        return false;
-    }
-    return true;
+/**
+ *Function to check user inputs and display errors.
+ *
+ * @param string @inputId
+ * @param string @errorId
+ * @param string @fieldName
+ * @param int @maxLength
+ * @return boolean.
+ */
+function validateInputs (inputId, errorId, fieldName, maxLength) {
+  var input = document.getElementById(inputId).value.trim();
+  var error = document.getElementById(errorId);
+  if (input == "") {
+    error.innerHTML = "* " + fieldName + " is required.<br>";
+    return false;
+  }
+  else if (!regex.test(input)) {
+    error.innerHTML = "* Only letters and white space allowed for " + fieldName + ".<br><br>";
+    return false;
+  }
+  else if (input.length >= maxLength) {
+    error.innerHTML = "* " + fieldName + " should not contain more than " + maxLength + " characters.<br><br>";
+    return false;
+  }
+  return true;
 }
 
-/* Validating user inputs on client side. */
+/**
+ * Validating user inputs on client side
+ *
+ * @returns boolean.
+ */
 function validate(){
-
-    var fnameValid = validateInputs("fname", "ferror", "First Name", 20);
-    var lnameValid = validateInputs("lname", "lerror", "Last Name", 20);
-    
-    return fnameValid && lnameValid;
-
+  var fnameValid = validateInputs("fname", "ferror", "First Name", 20);
+  var lnameValid = validateInputs("lname", "lerror", "Last Name", 20);
+  return fnameValid && lnameValid;
 }
