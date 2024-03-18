599259acec53a22d343dd3df7765e6f70b323f63
Md-Kaif-Innoraft <kaif.ansari@innoraft.com>
2024-03-18 13:04:22 +0530

    FT2-37-5: Modified code, converted to oops and added comments according to php standards.

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-// Regular expression for validation.
-
+/**
+ * Regular expression for validation.
+ */
 const regex = /^[a-zA-Z ]+$/;
 const numberRegex = /^[0-9]+$/;
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-/* Function to validate user inputs fields */
+/**
+ * Function to validate user input.
+ *
+ * @param string @inputId.
+ *  Input id of user input field.
+ * @param string @errorId.
+ *  Error id of input field.
+ * @param string @fieldName.
+ *  Field name of input field.
+ * @param int @maxLength.
+ *  Maximum length of input field.
+ * @param const @regex.
+ *  Regular expression to check input data.
+ * @return boolean.
+ */
 function validateInputs(inputId, errorId, fieldName, maxLength, regex) {
   var input = document.getElementById(inputId).value.trim();
   var error = document.getElementById(errorId);
@@ -23,7 +38,17 @@ function validateInputs(inputId, errorId, fieldName, maxLength, regex) {
   return true;
 }
 
-// Function to validate email input
+/**
+ * Function to validate user email.
+ *
+ * @param string @inputId.
+ *  Input id of user input field.
+ * @param string @errorId.
+ *  Error id of input field.
+ * @param string @fieldName.
+ *  Field name of input field.
+ * @return boolean.
+ */
 function validateEmail(inputId, errorId, fieldName) {
   var input = document.getElementById(inputId).value.trim();
   var error = document.getElementById(errorId);
@@ -31,13 +56,29 @@ function validateEmail(inputId, errorId, fieldName) {
     error.innerHTML = "* " + fieldName + " is required.<br>";
     return false;
     }
-    else if (!emailRegex.test(input)) { // Using emaiasdfdsalk@gmail.comlRegex for email validation
+    // Using email.Regex for email validation
+    else if (!emailRegex.test(input)) {
       error.innerHTML = "* Invalid " + fieldName + ". Please enter a valid email address.<br><br>";
       return false;
     }
     return true;
 }
 
+/**
+ * Function to validate user Phone Number.
+ *
+ * @param string @inputId.
+ *  Input id of user input field.
+ * @param string @errorId.
+ *  Error id of input field.
+ * @param string @fieldName.
+ *  Field name of input field.
+ * @param int @maxLength.
+ *  Maximum length of input field.
+ * @param const @regex.
+ *  Regular expression to check input data.
+ * @return boolean.
+ */
 function validateNum(inputId, errorId, fieldName, maxLength, regex) {
   var input = document.getElementById(inputId).value.trim();
   var error = document.getElementById(errorId);
@@ -55,8 +96,16 @@ function validateNum(inputId, errorId, fieldName, maxLength, regex) {
   }
   return true;
 }
-
-function validateMarks(inputId, errorId) {
+/**
+ * Function to validate user Marks.
+ *
+ * @param string @inputId.
+ *  Input id of user input field.
+ * @param string @errorId.
+ *  Error id of input field.
+ * @return boolean.
+ */
+function validateMarks (inputId, errorId) {
   var input = document.getElementById(inputId).value;
   var error = document.getElementById(errorId);
   var marksArray = input.split("\n");
@@ -65,7 +114,6 @@ function validateMarks(inputId, errorId) {
     error.innerHTML = "* Invalid Marks Entered.<br>";
     return false;
   }
-
   var isValid = false;
   for (var i = 0; i < marksArray.length; i++) {
     var marks = marksArray[i].split("|");
@@ -73,20 +121,23 @@ function validateMarks(inputId, errorId) {
       error.innerHTML = "* Invalid Marks Entered.<br>";
       return false;
     }
-    // Set isValid to true if at least one element is a number
+    // Set isValid to true if at least one element is a number.
     if (!isNaN(marks[0]) || !isNaN(marks[1])) {
       isValid = true;
     }
   }
 
-  if (!isValid) {
+  if (!isValid){
     error.innerHTML = "* At least one mark should be a non-empty string representing a number.<br>";
     return false;
   }
   return true;
 }
 
-/* Validating user inputs on client side. */
+/**
+ * Validating user inputs on client side.
+ * @return boolean.
+ */
 function validate() {
   var fnameValid = validateInputs("fname", "ferror", "First Name", 20, regex);
   var lnameValid = validateInputs("lname", "lerror", "Last Name", 20, regex);
@@ -99,3 +150,4 @@ function validate() {
   var validMarks = validateMarks("sub", "serror");
   return fnameValid && lnameValid && emailValid && numberValid && validMarks;
 }
+
--- index.php
+++ index.php
@@ -1,273 +1,88 @@
-<!-- PHP Logic starts here to fetch info  -->
 <?php
-
-  /* Include the User class file. */
-  require_once 'user.php';
-
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    /**
-     * Function to remove extra spaces,
-     * backslashes and convert special characters
-     * to HTML entites form user input data.
-     *
-     * @param string $data
-     *
-     * @return string $data
-     *
-     */
-    function checkInput($data) {
-      //Removes extra spaces from the begning and end.
-      $data = trim($data);
-      // Removes backslashes.
-      $data = stripslashes($data);
-      // convert special characters to HTML entities.
-      $data = htmlspecialchars($data);
-      return $data;
-    }
-    //Regex Pattern.
-    $regex = "/^[a-zA-Z-\s' ]*$/";
-    $numRegex = "/^[0-9]+$/";
-    $emailErr = $fnameErr = $lnameErr = $numErr = $emailSuccess = $emailMsg = "";
-
-    /**
-     * Checking user inputs.
-     *
-     * @param string $input
-     * Input from user.
-     *
-     * @param string $preg
-     * Regex to match pattern.
-     *
-     * @param &string $error
-     * Address of error @var to update.
-     *
-     * @param string $fieldName
-     * Field name of the input.
-     */
-
-    function validate($input, $preg, &$error, $fieldName) {
-      //Checking empty input.
-      if (empty ($input)) {
-        $error = "* $fieldName Name is required";
-        return "";
-      }
-      else {
-        $name = checkInput($input);
-        //Checking to Pattern from regex.
-        if (!preg_match($preg, $name)) {
-          $error = "* Invalid Input Type.";
-          return "";
-        }
-        //Checking empty @var name.
-        else if (empty ($name)) {
-          $error = "* Empty, Please enter valid $fieldName .";
-          return "";
-        }
-        else {
-          return $input;
-        }
-      }
-    }
-
-    /**
-     * Function checks email validation.
-     *
-     * @param string $email
-     * Input from user.
-     * @param string $emailErr
-     *
-     * @param string $emailSuccess
-     *
-     * @param string $emailMsg
-     *
-     * @return string $email
-     */
-
-    function validateEmail($email, &$emailErr, &$emailSuccess, &$emailMsg) {
-      //Check empty $email.
-      if (empty ($email)) {
-        $emailErr = "* Email is required";
-        return "";
-        }
-        else {
-          $email = checkInput($email);
-          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "* Invalid email format.";
-            return "";
-          }
-          else {
-            // Initialize a curl session.
-            $ch = curl_init();
-            //Set curl options.
-            curl_setopt($ch, CURLOPT_URL, 'https://emailvalidation.abstractapi.com/v1/?api_key=88e808712da846bf889378a04dd4d4f0&email=' . $email);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-            // Execute the curl request and store the response data.
-            $data = curl_exec($ch);
-            // Close the curl session.
-            curl_close($ch);
-            // Decode the JSON response data into an associative array.
-            $data = json_decode($data, true);
-            //Check if email deliverability is not "DELIVERABLE" and it's not a disposable email or has an invalid format.
-            if ($data['deliverability'] != "DELIVERABLE" && $data['is_disposable_email']['value'] != "false" && $data['is_valid_format']['text'] != "true") {
-              // Set an error message for invalid email
-              $emailErr = "* Invalid email.";
-              // Return an empty string to indicate validation failure.
-              return "";
-            }
-            // If email is valid, set success messages and return the email address.
-            $emailMsg = "Your email id is: $email";
-            $emailSuccess = "email validated successfully.";
-            return $email;
-          }
-        }
-
-    }
-
-    // Calling ValidatName function to validate name and store in variable.
-    $fname = validate($_POST["fname"], $regex, $fnameErr, "First Name");
-    $lname = validate($_POST["lname"], $regex, $lnameErr, "Last Name");
-    $num = validate($_POST["number"], $numRegex, $numErr, "Phone Number");
-    $email = validateEmail($_POST["email"], $emailErr, $emailSuccess, $emailMsg);
-
-    $numMsg = "";
-    //Checking Phone number is 10 digits or not.
-    if (!empty ($num) && strlen($num) == 10) {
-      $num = "+91 {$num}";
-      $numMsg = 'Your Phone Number is: ' . $num;
-    }
-    else {
-      $numErr = "Invalid Phone Number";
-    }
-
-    //Storing Image file.
-    if (!empty ($_FILES['image']['name'])) {
-      $img_temp_name = $_FILES['image']['tmp_name'];
-      $img_name = $_FILES['image']['name'];
-      move_uploaded_file($img_temp_name, "uploads/$img_name");
-    }
-
-    // Declearing and initializing var.
-    $tableErr = false;
-    $tableErrMsg = "";
-
-    //Checking if first name or last name is not empty.
-    if (!empty ($fname) && !empty ($lname)) {
-      //Creating an object .
-      $user = new User($fname, $lname);
-      $fullName = $user->getFname() . ' ' . $user->getLname();
-      $message = "Hello $fullName.";
-
-      //checking if marks field is not emptpy.
-      if (!empty ($_POST['sub'])) {
-        $marksArray = explode("\n", $_POST["sub"]);
-        $tableHead = "Marks";
-        $table = "<table class = 'table' border = '1' >";
-        $table .= "<tr><th>Subject</th><th>Marks</th></tr>";
-
-        // Loop through each subject and marks pair.
-        foreach ($marksArray as $mark) {
-          // Splitting subject and marks pair.
-          $marks = explode("|", $mark);
-          if (count($marks) == 2) {
-            // Adding table rows to the HTML string.
-            $table .= "<tr>";
-            if (is_numeric($marks[0])) {
-              $table .= "<td>$marks[1]</td>";
-              $table .= "<td>$marks[0]</td>";
-            }
-            else if (is_numeric($marks[1])) {
-              $table .= "<td>$marks[0]</td>";
-              $table .= "<td>$marks[1]</td>";
-            }
-            else {
-              $tableErr = true;
-            }
-            $table .= "</tr>";
-            }
-          else {
-            $tableErr = true;
-          }
-        }
-        // Close the HTML table tag.
-            $table .= "</table>";
-        }
-        else {
-          $tableErr = true;
-        }
-    }
-
-    //Checking if full name filed is not filled.
-    if (!empty ($_POST['fullName'])) {
-      $fullnameError = "* Invalid Input, You can't enter any value in Full Name.";
-    }
-    if ($tableErr == TRUE) {
-      $tableErrMsg = "* Invalid Marks Input.";
-    }
-  }
+  //Including formValidator.php file.
+  require 'formValidator.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignment 5</title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Assignment 5</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <div class="container">
-      <div class="box">
-        <!-- form starts here -->
-        <h2 class="text-center">PHP Assignment 5</h2><br><br>
+  <div class="container">
+    <div class="box">
+      <!-- form starts here -->
+      <h2 class="text-center">PHP Assignment 5</h2><br><br>
+      <form id="form" onsubmit = "return validate()" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
+        <label for="fName"><span class="error">* </span> First Name : </label> <br>
+        <input type="text" name="fName" id="fName" required maxlength="20" value="<?php echo $_POST['fName']; ?>" pattern="[a-zA-Z ]*">
+        <span class="error" id="ferror"><?php echo $formValidator->getFirstNameErr(); ?> <br><br></span>
 
-        <form id="form" onsubmit="return validate()" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
-        <label for="fname"><span class="error">* </span> First Name :</label> <br>
-        <input type="text" name="fname" id="fname" maxlength="20" value="<?php echo $_POST['fname']; ?>" required pattern="[a-zA-Z ]*">
-        <span class="error" id="ferror"><?php echo " $fnameErr <br><br>"; ?></span>
-
-        <label for="lname"> <span class="error">* </span> Last Name :</label> <br>
-        <input type="text" id="lname" name="lname" required maxlength="20" value="<?php echo $_POST['lname']; ?>"pattern="[a-zA-Z ]*"> <br>
-        <span class="error" id="lerror"><?php echo "$lnameErr <br><br>"; ?></span>
+        <label for="lName"> <span class="error">* </span> Last Name : </label> <br>
+        <input type="text" id="lName" name="lName" required maxlength="20" value="<?php echo $_POST['lName']; ?>" pattern="[a-zA-Z ]*"> <br>
+        <span class="error" id="lerror"><?php echo $formValidator->getLastNameErr(); ?> <br><br></span>
 
         <label for="number"> Phone number : </label> <br><input type="text" id="number" maxlength="10" name="number" value="<?php echo $_POST['number']; ?>" required pattern="[0-9]*"> <br>
-        <span class="error" id="nerror"><?php echo "$numErr <br> <br>"; ?></span>
+        <span class="error" id="nerror"><?php echo $formValidator->getNumErr(); ?><br> <br></span>
 
-        <label for="email"><span class="error">* </span> Email : <span class="success"><?php echo "$emailSuccess"; ?></span></label> <br>
+        <label for="email"><span class="error">* </span> Email : <span class="success"><?php echo $formValidator->getEmailSuccess(); ?></span></label> <br>
         <input type="text" id="email" name="email"value="<?php echo $_POST['email']; ?>" required> <br>
-        <span class="error" id="emailErr"><?php echo "$emailErr <br> <br>"; ?></span>
+        <span class="error" id="emailErr"><?php echo $formValidator->getEmailErr(); ?> <br> <br></span>
 
         <label for="sub">Subjects and Marks : ( Format: English|80)</label>
         <textarea required name="sub" id="sub" rows="3"></textarea>
-        <span class="error" id="serror"><?php echo $tableErrMsg; ?><br><br></span>
+        <span class="error" id="serror"><?php echo $formValidator->getTableErr(); ?><br><br></span>
 
         <label for="image">Image :</label> <br>
-        <input class="file" type="file" id="image" name="image" accept="image/png, image/gif, image/jpeg"><br><br>
+        <input class="file" type="file" name="image" accept="image/png, image/gif, image/jpeg"><br><br>
 
-        <button class="btn">Submit</button> <br> <br> <br>
+        <button class="btn">Submit </button> <br> <br> <br>
 
         <label for="fullName">Full Name:</label> <br>
-        <input type="text" id="fullName" name="fullName" disabled value="<?php echo $fullName; ?>">
-        <span class="error"><?php echo $fullnameError; ?></span>
-        <br><br>
-        </form>
-        <!-- Form ends here. -->
-        <!-- Print full name Greeting message and Image. -->
-        <div class="img-sec">
+        <input type="text" id="fullName" name="fullName" disabled value="<?php echo $formValidator->getFullName(); ?>">
+        <span class="error"><?php echo $formValidator->getFullNameError(); ?></span> <br><br>
+      </form>
+      <!-- form ends here -->
+      <!-- Printing full name with greeting And Image. -->
+      <div class="img-sec">
           <?php
-            if (isset ($fullName)) {
-              echo $message; ?> <br>
-              <?php echo $numMsg; ?><br>
-              <?php echo $emailMsg; ?><br> <br>
-              <?php if (!empty ($img_name)) { ?>
-              <img src='uploads/<?php echo $img_name; ?>' height=250px> <br>
-              <?php echo $img_name; ?><br><br>
-              <?php } echo $table; }
+            if (!empty($formValidator->getFullName())) {
+              echo $formValidator->getMessage(); ?> <br> <br>
+              <?php echo $formValidator->getNumMsg(); ?><br>
+              <?php echo $formValidator->getEmailMsg(); ?><br> <br>
+              <?php if (!empty($formValidator->getImgName())) { ?>
+              <img src = 'uploads/<?php echo $formValidator->getImgName(); ?>' height= 250px > <br>
+              <?php echo $formValidator->getImgName();
+              }
+              if (empty($formValidator->getTableErr())) { ?>
+                <h2>Marks</h2>
+                <table class = 'table' border = '1'>
+                  <thead>
+                    <tr>
+                      <th>Subject</th>
+                      <th>MArks</th>
+                    </tr>
+                  </thead>
+                  <?php foreach ($formValidator->getMarksTable() as $subject => $marks) { ?>
+                    <tr>
+                      <td><?php echo $subject; ?></td>
+                      <td><?php echo $marks; ?></td>
+                    </tr>
+                  <?php } ?>
+                </table>
+              <?php
+              }
+            }
           ?>
-        </div>
       </div>
-    <script src="index.js"></script>
+    </div>
+  </div>
+  <script src="index.js"></script>
 </body>
 </html>
+
